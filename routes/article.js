1d4c9d1a264997506eba3ff35fd8104a47abfdb6
suoling <wangsuoling@163.com>
2019-11-20 18:27:59 +0800

    图片上传摸索

--- routes/article.js
+++ routes/article.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const article = require('../modules/handle/article');
 
 // 写文章
-router.post('/write', function(req, res, next) {
-  article.write(req, res, next);
+router.post('/publish', function(req, res, next) {
+  article.publish(req, res, next);
+});
+
+// 查询文章列表
+router.post('/query', function(req, res, next) {
+  article.query(req, res, next);
+});
+
+// 图片上传
+router.post('/img/upload', function(req, res, next) {
+  article.imgUpload(req, res, next);
 });
 
 module.exports = router;
